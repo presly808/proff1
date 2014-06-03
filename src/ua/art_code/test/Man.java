1c84f2af8d591bfe0228cbdc4c09f81361ec61eb
presly808 <presly808@gmail.com>
2014-06-03 08:28:44 +0300

    First Comm

---
+++ src/ua/art_code/test/Man.java
@@ -0,0 +1,19 @@
+package ua.art_code.test;
+
+/**
+ * Created by admin on 03.06.2014.
+ */
+public class Man {
+
+    private String name;
+    private int age;
+
+    public Man(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void some(){
+        System.out.println("Say hello somme");
+    }
+}
