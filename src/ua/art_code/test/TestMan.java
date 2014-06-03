77bf9ab190e41e165f8c9a3fa723015c5409a28b
presly808 <presly808@gmail.com>
2014-06-03 09:35:44 +0300

    update gitignore

---
+++ src/ua/art_code/test/TestMan.java
@@ -0,0 +1,11 @@
+package ua.art_code.test;
+
+/**
+ * Created by admin on 03.06.2014.
+ */
+public class TestMan {
+    public static void main(String[] args) {
+        Man man = new Man("Vova", 12);
+        System.out.println(man);
+    }
+}
